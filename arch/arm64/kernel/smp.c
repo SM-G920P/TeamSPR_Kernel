5dd523a25fb117d8f5ad8b40e5c9aba9bd42b611
Tony Malagisi <tvm2487@gmail.com>
2016-02-22 14:46:22 -0500

    Revert "arm64: smp: Update the topology masks before calling CPU_STARTING notifiers"
    This reverts commit c7a2535fa4c7ba7b71741f7be1adabd929386337.

--- arch/arm64/kernel/smp.c
+++ arch/arm64/kernel/smp.c
@@ -163,9 +163,9 @@ asmlinkage void __cpuinit secondary_start_kernel(void)
 	/*
 	 * Enable GIC and timers.
 	 */
-	smp_store_cpu_info(cpu);
-
 	notify_cpu_starting(cpu);
+
+	smp_store_cpu_info(cpu);
 
 	/*
 	 * OK, now it's safe to let the boot CPU continue.  Wait for
